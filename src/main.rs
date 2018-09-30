4ed492acbc57081a799b256b9f4cf90efcdd1206
iyuroch <iyuroch@gmail.com>
2018-09-30 15:19:06 +0300

    Fix not correct child arguments, add cd
    In previous version there was problem due to close read end of pipe - if that occured, ancestor of command had
    problem because of it. Now we handle this error such way, that we kill ancestor command if successors had problems
    being executed (due to bad name of executable or arguments).
    Cd implement as well as changed prompt.

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,62 @@ use std::collections::VecDeque;
 use std::os::unix::io::RawFd;
 use std::process;
 
-use nix::unistd::{fork, ForkResult, execvp, pipe, dup2, close, Pid};
-use nix::sys::wait::{wait, waitpid, WaitPidFlag, WaitStatus};
+use nix::unistd::{getcwd, chdir, fork, ForkResult, execvp, pipe, dup2, close, Pid};
+use nix::sys::wait::{waitpid};
+use nix::sys::signal::{kill, SIGKILL};
 
-// TODO: print pwd
+// TODO: implement result for cd
 // TODO: globs
 // TODO: add cd to this!!
 // TODO: find autocompletion
-// TODO: iterator isntead of for loop
+// TODO: iterator instead of for loop
+// TODO: make option from prompt
 
 // as we pass fd to the children - they still remain opened in the parent process,
 // those cannot be used in children. if we close them in parent - we cannot pass them to the children
 // as they remain closed. we can reopen them in child process, or close at the very end
 
-fn spawn_proc(str_argv: &str, fd_arr: VecDeque<Option<RawFd>>, last: Option<()>) {
+fn cd(args: Vec<CString>) {
+    if args.len() > 2 {
+        println!("too much arguments");
+        return;
+    }
+    match chdir(*&args[1].to_str().unwrap()) {
+        Ok(_) => {},
+        Err(e) => println!("cannot cd {:?}", e),
+    }
+}
+
+fn spawn_proc(str_argv: &str, fd_arr: VecDeque<Option<RawFd>>, 
+        last: Option<()>, first_child: &mut Option<Pid>) {
+    
+    // might crash if bad arguments
+    let cstr_argv: Vec<_> = str_argv.split(" ")
+                                    .map(|arg| CString::new(arg).unwrap())
+                                    .collect();
+    
+    // some commands are implemented as functions
+    let exec_name = &cstr_argv[0].clone().into_string().unwrap();
+    match exec_name.as_ref() {
+        "cd" => {
+            cd(cstr_argv);
+            return;
+        },
+        _ => {},
+    }
+
+    
     let (parent_write_end, read_end, write_end, child_read_end) 
     = (fd_arr[0], fd_arr[1], fd_arr[2], fd_arr[3]);
     match fork() {
         Ok(ForkResult::Parent { child, .. }) => {
+
+            match *first_child {
+                Some(_) => {},
+                None => {
+                    *first_child = Some(child);
+                }
+            }
 
             match parent_write_end {
                 Some(v) => {
@@ -86,29 +124,47 @@ fn spawn_proc(str_argv: &str, fd_arr: VecDeque<Option<RawFd>>, last: Option<()>)
                     None => {},
             }
 
-            // might crash if bad arguments
-            let cstr_argv: Vec<_> = str_argv.split(" ")
-                                    .map(|arg| CString::new(arg).unwrap())
-                                    .collect();
-
             let raw_argv = cstr_argv.into_boxed_slice();
 
             match execvp(&raw_argv[0], &*raw_argv) {
                 Ok(_) => {},
                 Err(_) => {
-                    println!("Cannot execute command");
+                    println!("cannot execute command {:?}", &raw_argv[0]);
+                    match *first_child {
+                        Some(v) => {
+                            match kill(v, SIGKILL) {
+                                Ok(_) => {},
+                                Err(e) => {
+                                    println!("cannot kill pid {:?}", e);
+                                },
+                            };                            
+                        },
+                        None => {},
+                    }
                     process::exit(0x0100);
                 }
             };
+
+            return;
         },
-        Err(_) => println!("Fork failed"),
+        Err(_) => {
+            println!("Fork failed");
+            return;
+        },
     }
+}
+
+fn prompt() {
+    let cwd = String::from(getcwd().unwrap().to_str().unwrap());
+    let mut prompt_string = cwd.into_bytes();
+    prompt_string.extend_from_slice(" > ".as_bytes());
+    io::stdout().write(&prompt_string).expect("Cannot print");
+    io::stdout().flush().expect("Cannot flush output");
 }
 
 fn main() {
     let mut stdin_buff = String::new();
-    io::stdout().write(b"cmd >>").expect("Cannot print output");
-    io::stdout().flush().expect("Cannot flush output");
+    prompt();
 
     loop {
         io::stdin().read_line(&mut stdin_buff).expect("Cannot read input");
@@ -127,6 +183,8 @@ fn main() {
 
             let mut fd_arr: VecDeque<_> = vec![None, None].into_iter().collect();
             let mut last: Option<()> = None;
+            // we need to remember first child pid - if we can't run some of 
+            // the successsors - we need to kill first child not to have pipe error
             let mut first_child: Option<Pid> = None;
 
             while let Some(pr_args) = pr_arr.pop_front() {
@@ -137,14 +195,11 @@ fn main() {
                     let (read_end, write_end) = pipe().unwrap();
                     fd_arr.extend(&[Some(write_end.clone()), Some(read_end.clone())]);
                 }
-                // println!("{:?}", fd_arr);
-                spawn_proc(pr_args, fd_arr.clone(), last);
+                spawn_proc(pr_args, fd_arr.clone(), last, &mut first_child);
                 fd_arr = fd_arr.split_off(2);
             }
-            // spawn_proc(pr_arr.pop_front(), &mut pr_arr, None, None, true);
         }
-        io::stdout().write(b"cmd >>").expect("Cannot print output");
-        io::stdout().flush().expect("Cannot flush output");
+        prompt();
         stdin_buff.clear();
     }
 }
